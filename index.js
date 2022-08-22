07b8347c936952ed529b91ba0797129227478c46
amishagin <mishagin.2012@mail.ru>
2022-08-22 17:59:43 +0500

    Almost final version

--- index.js
+++ index.js
@@ -2,34 +2,57 @@
 //let counter = 0;
 let todo = [];
 let selectedItem;
+let selectedItemIndex = selectedItem?.dataset?.index;
 
 window.onload = () => {
     todo = JSON.parse(window.localStorage.getItem('todo'));
     render();
 }
 
+
+
 const render = () => {
-    let list = document.getElementsByClassName("todo-list")[0];
+    let list = document.getElementsByClassName("Todo-list")[0];
     let dataIndex = 0;
-    list.innerHTML = "";
+
+    //console.log(selectedItemIndex);
+    list.replaceChildren();
+
     for (let item of todo) {
+        //Создание li
         let li = document.createElement("li");
         li.textContent = item.text;
         li.style.backgroundColor = item.color != undefined ? item.color : "";
-        li.className = "todo-list item";
-        li.setAttribute("data-index", dataIndex++);
-
+        li.className = "Todo-list item";
+        li.setAttribute("data-index", dataIndex);
+        // Создание checkbox
         let input = document.createElement("input");
         input.type = "checkbox";
-        input.className = "todo-list item check";
+        input.className = "Todo-list item check";
         input.checked = item.done;
+        // Обработка checked
         input.onchange = (e) => {
             item.done = input.checked;
-            console.log(item);
+            //console.log(item);
             saveToStorage();
         };
+
         li.append(input);
         list.append(li);
+
+        //Задание активного элемента при перерендере
+        if (selectedItem) {
+            if (parseInt(selectedItemIndex) == dataIndex) setActive(li);
+        }
+        dataIndex++;
+    }
+    //TODO Добавить адекватный обработчик checkbox
+    let input = document.getElementsByClassName('checked');
+    input.onchange = (e) =>{
+        let item = e.target.closest('Todo-list item');
+        item.done = input.checked;
+        //console.log(item);
+        saveToStorage();
     }
     // Обработчик нажатия по списку
     list.onclick = function (e) {
@@ -39,73 +62,118 @@ const render = () => {
     };
 };
 
+
+
 // Удалить элемент
 const deleteTodo = () => {
     if (selectedItem) todo.splice(selectedItem.dataset.index, 1);
     else todo.pop;
+
     saveToStorage();
     render();
 };
+
+
 
 // Добавить элемент
 const addTodo = () => {
     let text = window.prompt("Что хотите сделать?", "");
+
     if (!text) return;
-    //let color = window.prompt("Какой цвет", "");
+
     let elem = { text: text, done: false };
+
     if (selectedItem)
         todo.splice(parseInt(selectedItem.dataset.index) + 1, 0, elem);
     else todo.push(elem);
+
     saveToStorage();
     render();
 };
+
+
 
 // Поднять элемент
 const moveUpTodo = () => {
     if (!selectedItem) return;
+
     if (0 == selectedItem.dataset.index) {
         alert("Куда выше то?");
         return;
     }
-    let temp = todo[parseInt(selectedItem.dataset.index) - 1];
-    todo[parseInt(selectedItem.dataset.index) - 1] =
-        todo[selectedItem.dataset.index];
-    todo[selectedItem.dataset.index] = temp;
+
+    let index = parseInt(selectedItem.dataset.index);
+    let temp = todo[index - 1];
+
+    todo[index - 1] = todo[index];
+    todo[index] = temp;
+    selectedItemIndex = index - 1;
+
     saveToStorage();
     render();
 };
 
+
+
 // Опустить элемент
 const moveDownTodo = () => {
     if (!selectedItem) return;
+
     if (todo.length - 1 == selectedItem.dataset.index) {
         alert("Куда ниже то?");
         return;
     }
-    let temp = todo[parseInt(selectedItem.dataset.index) + 1];
-    todo[parseInt(selectedItem.dataset.index) + 1] =
-        todo[selectedItem.dataset.index];
-    todo[selectedItem.dataset.index] = temp;
+    let index = parseInt(selectedItem.dataset.index);
+    let temp = todo[index + 1];
+
+    todo[index + 1] = todo[index];
+    todo[index] = temp;
+    //selectedItemIndex++;
+    selectedItemIndex = index + 1;
+
     saveToStorage();
     render();
 };
+
+
 // Изменить элемент
 const editTodo = () => {
-    if(!selectedItem) return;
-    let div = document.getElementsByClassName('input-props')[0];
-    div.removeAttribute('hidden');
-    let button = document.getElementsByClassName('props-change')[0];
-    button.onclick = () => {
-        let inputName = document.getElementsByClassName('input-name')[0];
-        let inputColor = document.getElementsByClassName('input-color')[0];
-        if(!inputName) return;
-        todo[selectedItem.dataset.index].text = inputName.value;
-        todo[selectedItem.dataset.index].color = inputColor.value;
+    if (!selectedItem) return;
+
+    let div = document.getElementsByClassName('Input-props')[0];
+    div.style.display = 'flex';
+
+    let buttonChange = document.getElementsByClassName('Button props')[0];
+
+    buttonChange.onclick = () => {
+        let inputName = document.getElementsByClassName('Input name')[0];
+        let inputColor = document.getElementsByClassName('Input color')[0];
+
+        if (!inputName) return;
+
+        let index = parseInt(selectedItem.dataset.index);
+
+        todo[index].text = inputName.value != "" ? inputName.value : todo[index].text;
+        todo[index].color = inputColor.value;
+
+
+        div.style.display = 'none';
+        inputName.value = '';
+        inputColor.value = '';
+
         saveToStorage();
-        div.setAttribute('hidden','');
         render();
     }
+
+    let buttonCancel = document.getElementsByClassName('Button props')[1];
+
+    buttonCancel.onclick = () => {
+        div.style.display = 'none';
+        return;
+    }
 }
+
+
 
 // Сделать элемент активным
 function setActive(li) {
@@ -116,7 +184,11 @@ function setActive(li) {
     selectedItem.classList.add("active");
     //console.log(selectedItem);
 }
-// Сохранить в ToDo в LocalStorage
+
+
+// Сохранить ToDo в LocalStorage
 function saveToStorage() {
     window.localStorage.setItem('todo', JSON.stringify(todo))
 }
+
+
